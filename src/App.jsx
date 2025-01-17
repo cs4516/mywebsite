1829dc051bd40fdf5d7bc1ca587c9f98e0c72c0a
cs4516 <cristiansicha06@gmail.com>
2025-01-17 00:15:33 -0500

    WE good

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import backgroud_img from './assets/Ecuadorpic.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more or not
-      </p>
+      <nav>
+        <div>
+          <ul id="navbar">
+            <li><a href="#/Home"><Home/></a></li>
+            <li><a href="#/Experience"><Experience/></a></li>
+            <li><a href="#/VolunteerWork"><Volunteer/></a></li>
+            <li><a href="#/Projects"><Projects/></a></li>
+          </ul>
+        </div>
+      </nav> 
+
+      <img src={backgroud_img} alt="ecuador" className = "image1" />
+
+      <h1 id="Heading">
+        Hello! my name is Cristian
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
